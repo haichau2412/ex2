58148aac4e3ae7bf1cb8e0d545a445c1307cf5a8
haichau2412 <chau96cc@gmail.com>
2020-07-08 15:28:47 +0700

    change text

--- src/containers/userdashboard/Users.js
+++ src/containers/userdashboard/Users.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch } from "react-redux";
 const Users = () => {
   const dispatch = useDispatch();
-  return <div onClick={() => dispatch({ type: "LOG_OUT" })}>Users</div>;
+  return <div onClick={() => dispatch({ type: "LOG_OUT" })}>Log out</div>;
 };
 
 export default Users;
